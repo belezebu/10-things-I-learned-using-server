4ba01d0eb14306e15e3e6b394aebd9f8108b2369
Paulo Andrade <paulo.andrade@mindera.com>
2021-03-20 03:06:35 +0000

    Add middy and other middlewares

--- demo/src/handlers/helloWorldHandler.ts
+++ demo/src/handlers/helloWorldHandler.ts
@@ -1,5 +1,11 @@
-import {APIGatewayEvent, APIGatewayProxyResult} from "aws-lambda";
+import {APIGatewayProxyResult} from "aws-lambda";
 import {init, lambdaWrapper} from 'epsagon'
+import middy from '@middy/core'
+import httpErrorHandler from '@middy/http-error-handler'
+import captureCorrelationIds from '@dazn/lambda-powertools-middleware-correlation-ids'
+import Logger from '@dazn/lambda-powertools-logger'
+import flow from 'lodash.flow'
+import {createAPIResponse} from "../utils/utils";
 
 init({
     token: process.env.EPSAGON_TOKEN,
@@ -7,18 +13,9 @@ init({
     metadataOnly: false,
 });
 
-const createAPIResponse = (
-    statusCode: number,
-    body: any
-): APIGatewayProxyResult => ({
-    statusCode,
-    body: typeof body === "string" ? body : JSON.stringify(body)
-});
-
-const helloWorld = async (
-    event: APIGatewayEvent
-): Promise<APIGatewayProxyResult> => {
-    return createAPIResponse(200, {ok: true});
+const helloWorldHandler = async (): Promise<APIGatewayProxyResult> => {
+    Logger.info('Returning Hello World Response')
+    return createAPIResponse(200, {message: 'Hello World'});
 }
 
-export default lambdaWrapper(helloWorld)
+export default flow([lambdaWrapper])(middy(helloWorldHandler).use(captureCorrelationIds({sampleDebugLogRate: 1.0})).use(httpErrorHandler()))
---
+++ demo/src/utils/utils.ts
@@ -0,0 +1,9 @@
+import {APIGatewayProxyResult} from "aws-lambda";
+
+export const createAPIResponse = (
+    statusCode: number,
+    body: any
+): APIGatewayProxyResult => ({
+    statusCode,
+    body: typeof body === "string" ? body : JSON.stringify(body)
+});
